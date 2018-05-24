0c4955f13760c5261e35c9447fdea210406ffbe6
Alexander Ursu <alexander.ursu@gmail.com>
2018-05-24 23:07:57 +0200

    Added check for new RPM packages

--- createrepo/scripts/run.sh
+++ createrepo/scripts/run.sh
@@ -9,5 +9,10 @@ if ! test $DELAY_PERIOD -gt 0 2>/dev/null; then
     DELAY_PERIOD=60
 fi
 
-/usr/bin/createrepo .
+# check if any modified packages during last half day
+if find . -mtime -12 -name *.rpm | grep -q rpm; then
+    /usr/bin/createrepo .
+    find . -name *.rpm -exec touch -d yesterday {} \;
+fi
+
 sleep $DELAY_PERIOD
